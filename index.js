c7b1b74ef418edfdc137ca227d92907f78e6897e
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2020-04-26 20:00:15 -0300

    Refatoração do código e comentários

--- index.js
+++ index.js
@@ -5,31 +5,70 @@ const path = require('path');
 
 const [, , ...userInputArguments] = process.argv;
 
-// Name of the component and file extension passed bt user as an argument
+// Component name and file extension passed as argument by the user
 const [componentName, fileExtension = 'ts'] = userInputArguments;
 
+// Settings of the files that will be created
+const filesToCreate = [
+  {
+    templateFile: 'Component.tsx',
+    outputFileName: `${componentName}.${fileExtension}x`,
+  },
+  {
+    templateFile: 'Component.style.ts',
+    outputFileName: `${componentName}.${fileExtension}`,
+  },
+  {
+    templateFile: 'index.ts',
+    outputFileName: `index.${fileExtension}`,
+  },
+];
+
+// Function for creating component directory
 const createComponentDirectory = () => fs.mkdirSync(`./${componentName}`);
 
+// Function for creating component files
 const createComponentFile = (fileName, fileContent) =>
   fs.writeFileSync(`./${componentName}/${fileName}`, fileContent);
 
+// Change the variable @FILE_NAME@ to the component name passed by the user
 const replaceTemplateFileData = (fileData) =>
   fileData.replace(/@FILE_NAME@/gim, componentName);
 
-const createFileFromTemplate = (templateFile, outputFile) => {
+// Create files based on templates
+const createFileFromTemplate = (templateFile, outputFileName) => {
   fs.readFile(
     path.resolve(__dirname, 'templates', templateFile),
     'utf8',
     (error, fileData) => {
-      error && console.error('Houston, we have a problem:', error);
+      if (error) {
+        console.error('Houston, we have a problem:', error);
+        return;
+      }
 
-      const newFileData = replaceTemplateFileData(fileData);
+      const outputFileContent = replaceTemplateFileData(fileData);
 
-      createComponentFile(outputFile, newFileData);
+      createComponentFile(outputFileName, outputFileContent);
     }
   );
 };
 
+// Function to create component folder and files
+const createComponent = () => {
+  try {
+    createComponentDirectory();
+
+    filesToCreate.map(({ templateFile, outputFileName }) => {
+      createFileFromTemplate(templateFile, outputFileName);
+    });
+
+    console.log(`Finished! The <${componentName} /> was created.`);
+  } catch (error) {
+    console.error('Houston, we have a problem:', error);
+  }
+};
+
+// Start application
 const start = () => {
   console.log('Starting...');
 
@@ -38,32 +77,14 @@ const start = () => {
     return;
   }
 
-  try {
-    createComponentDirectory();
+  fs.exists(componentName, (exists) => {
+    if (exists) {
+      console.error('This component already exists.');
+      return;
+    }
 
-    createFileFromTemplate(
-      'Component.tsx',
-      `${componentName}.${fileExtension}x`
-    );
-
-    createFileFromTemplate(
-      'Component.style.ts',
-      `${componentName}.${fileExtension}`
-    );
-
-    createFileFromTemplate('index.ts', `index.${fileExtension}`);
-
-    console.log(`Finished! The <${componentName} /> was created.`);
-  } catch (error) {
-    console.error('Houston, we have a problem:', error);
-  }
+    createComponent();
+  });
 };
 
-fs.exists(componentName, (exists) => {
-  if (exists) {
-    console.error('This component already exists.');
-    return;
-  }
-
-  start();
-});
+start();
